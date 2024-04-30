a0312c2ceded23eaf6ede4d5c445704f31223ca9
Mehmet <86670547+mmtdgn@users.noreply.github.com>
2024-04-30 11:54:42 +0300

    Update ObjectInventory.cs

--- ObjectInventory.cs
+++ ObjectInventory.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 using Object = UnityEngine.Object;
 using UnityEditor.SceneManagement;
 
-namespace S.TD
+namespace MD.Editor.Tools
 {
     public class ObjectInventory : EditorWindow
     {
